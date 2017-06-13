ba4d0b1859fefc7f0b0187a3fb517b7e6326ce22
Bryan Orabutt <borabut@bibliophobia.exp.ee.siue.edu>
2017-06-13 16:27:10 -0500

    Formatted rollout.sh better

--- rollout.sh
+++ rollout.sh
@@ -2,12 +2,12 @@
 
 echo "Updating /etc/rc.conf..."
 sudo cp rc.conf /etc/rc.conf
-echo "\nUpdating /usr/local/etc/httpd.conf..."
+echo "Updating /usr/local/etc/httpd.conf..."
 sudo cp httpd.conf /usr/local/etc/httpd.conf
-echo "\nUpdating /usr/local/etc/dhcpd577.conf..."
+echo "Updating /usr/local/etc/dhcpd577.conf..."
 sudo cp dhcpd577.conf /usr/local/etc/dhcpd577.conf
-echo "\nUpdating /usr/local/namedb/named.conf..."
+echo "Updating /usr/local/namedb/named.conf..."
 sudo cp named.conf /usr/local/etc/namedb/named.conf
-echo "\nUpdating /etc/resolv.conf..."
+echo "Updating /etc/resolv.conf..."
 sudo cp resolv.conf /etc/resolv.conf
-echo "\nYou are up to date!"
+echo "You are up to date!"
