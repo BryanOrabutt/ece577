8f22e3236cfc1974f5e0cbd989a23a882bf31d62
Bryan Orabutt <borabut@bibliophobia.exp.ee.siue.edu>
2017-06-13 16:18:38 -0500

    Fixed rollout.sh

--- rollout.sh
+++ rollout.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "Updating /etc/rc.conf..."
-mv rc.conf /etc/rc.conf
+sudo cp rc.conf /etc/rc.conf
 echo "\nUpdating /usr/local/etc/httpd.conf..."
-mv httpd.conf /usr/local/etc/httpd.conf
-echo "\nUpdating /usr/local/etc/dhcp577.conf..."
-mv dhcp577.conf /usr/local/etc/dhcp577.conf
+sudo cp httpd.conf /usr/local/etc/httpd.conf
+echo "\nUpdating /usr/local/etc/dhcpd577.conf..."
+sudo cp dhcpd577.conf /usr/local/etc/dhcpd577.conf
 echo "\nUpdating /usr/local/namedb/named.conf..."
-mv named.conf /usr/local/etc/namedb/named.conf
+sudo cp named.conf /usr/local/etc/namedb/named.conf
 echo "\nUpdating /etc/resolv.conf..."
-mv resolv.conf /etc/resolv.conf
+sudo cp resolv.conf /etc/resolv.conf
 echo "\nYou are up to date!"
